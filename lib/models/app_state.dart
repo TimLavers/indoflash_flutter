7024cfa1c05965557849ff7648d3f8a1aca52e29
TimLavers <tim@grandtestauto.org>
2018-03-18 11:26:46 +1100

    Initial upload

---
+++ lib/models/app_state.dart
@@ -0,0 +1,33 @@
+import 'package:meta/meta.dart';
+
+enum Screen {
+  word_list, selecting_word_list, selecting_chapter
+}
+
+@immutable
+class AppState {
+  final Screen screen;
+
+  AppState({this.screen = Screen.word_list});
+
+  factory AppState.showingWordList() => new AppState(screen: Screen.word_list);
+
+  AppState copyWith({Screen screen}) {
+    return new AppState(screen: screen);
+  }
+
+  @override
+  int get hashCode => screen.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is AppState &&
+              runtimeType == other.runtimeType &&
+              screen == other.screen;
+
+  @override
+  String toString() {
+    return 'AppState{screen: $screen}';
+  }
+}
---
+++ test/models/app_state_test.dart
@@ -0,0 +1,53 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_app2/models/app_state.dart';
+
+main() {
+  group('Constructors and factories', () {
+    test('constructor empty', () {
+      AppState state = new AppState();
+      expect(state.screen, Screen.word_list);
+    });
+
+    test('constructor with screen', () {
+      AppState state = new AppState(screen: Screen.selecting_chapter);
+      expect(state.screen, Screen.selecting_chapter);
+    });
+
+    test('showingWordList', () {
+      AppState state = new AppState.showingWordList();
+      expect(state.screen, Screen.word_list);
+    });
+  });
+
+  group('copier', () {
+    test('copyWith', () {
+      AppState state = new AppState.showingWordList();
+      var copyWith = state.copyWith(screen: Screen.selecting_word_list);
+      expect(copyWith.screen, Screen.selecting_word_list);
+    });
+  });
+
+  group('standard', () {
+    test('==', () {
+      AppState selectingChapter0 = new AppState(screen: Screen.selecting_chapter);
+      AppState selectingChapter1 = new AppState(screen: Screen.selecting_chapter);
+      AppState selectingWordlist = new AppState(screen: Screen.selecting_word_list);
+      AppState wordList = new AppState(screen: Screen.word_list);
+      expect(selectingChapter0 == selectingChapter0, true);
+      expect(selectingChapter0 == selectingChapter1, true);
+      expect(selectingChapter0 == selectingWordlist, false);
+      expect(selectingWordlist == wordList, false);
+    });
+
+    test('hashCode', () {
+      AppState selectingChapter0 = new AppState(screen: Screen.selecting_chapter);
+      AppState selectingChapter1 = new AppState(screen: Screen.selecting_chapter);
+      expect(selectingChapter0.hashCode, selectingChapter1.hashCode);
+    });
+
+    test('toString', () {
+      AppState selectingChapter = new AppState(screen: Screen.selecting_chapter);
+      expect(selectingChapter.toString(), 'AppState{screen: Screen.selecting_chapter}');
+    });
+  });
+}
