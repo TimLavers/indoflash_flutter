e437b4ef6c6b3be9b4670cae08e3526c20827584
TimLavers <tim@grandtestauto.org>
2018-04-23 06:59:59 +1000

    Fat arrows.

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -1,16 +1,19 @@
 //Adapted from https://github.com/brianegan/flutter_architecture_samples.git
-import 'package:meta/meta.dart';
 import 'package:flutter_app2/spec/spec.dart';
+import 'package:meta/meta.dart';
 
 enum Screen {
-  word_list, selecting_word_list, selecting_chapter
+  word_list,
+  selecting_word_list,
+  selecting_chapter
 }
 
 typedef void ChapterSelected(int index);
 
 @immutable
 class AppState {
-  final ApplicationSpec applicationSpec = new ChapterStructure().applicationSpec;
+  final ApplicationSpec applicationSpec = new ChapterStructure()
+      .applicationSpec;
   final Screen screen;
   final ChapterSpec currentChapter;
 
@@ -18,11 +21,10 @@ class AppState {
 
   AppState.copy(this.currentChapter, this.screen);
 
-  AppState copyWith(Screen screen) {
-    return new AppState.copy(currentChapter,screen);
-  }
+  AppState copyWith(Screen screen) => new AppState.copy(currentChapter, screen);
 
-  AppState copyWithChapter(int chapterIndex) => new AppState.copy(applicationSpec.chapters[chapterIndex], this.screen);
+  AppState copyWithChapter(int chapterIndex) =>
+      new AppState.copy(applicationSpec.chapters[chapterIndex], this.screen);
 
   @override
   int get hashCode => screen.hashCode;
@@ -36,7 +38,6 @@ class AppState {
               screen == other.screen;
 
   @override
-  String toString() {
-    return 'AppState{screen: $screen, chapter: ${currentChapter.title}';
-  }
+  String toString() =>
+      'AppState{screen: $screen, chapter: ${currentChapter.title}';
 }
