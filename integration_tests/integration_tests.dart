a35481ed3184d97d1b40c10491e1cf0687811d00
TimLavers <tim@grandtestauto.org>
2018-05-06 14:58:49 +1000

    A couple more integration tests.

--- integration_tests/integration_tests.dart
+++ integration_tests/integration_tests.dart
@@ -26,9 +26,61 @@ main() {
       }
     });
 
-    test('initial state', () async {
+    test('next word', () async {
       expect(await homeScreen.currentWord, 'anda');
       expect(await homeScreen.currentDefinition, '');
+      await homeScreen.tapNavigationButton();
+      expect(await homeScreen.currentWord, 'anda');
+      expect(await homeScreen.currentDefinition, 'you');
+
+      await homeScreen.tapNavigationButton();
+      expect(await homeScreen.currentWord, 'apa');
+      expect(await homeScreen.currentDefinition, '');
+      await homeScreen.tapNavigationButton();
+      expect(await homeScreen.currentWord, 'apa');
+      expect(await homeScreen.currentDefinition, 'what');
+    });
+    
+    test('repeat list when finished', () async {
+      //There are 24 words in the first list. Go through the first 23.
+      for (int i=0; i<22; i++) {
+        await homeScreen.tapNavigationButton();
+        await homeScreen.tapNavigationButton();
+      }
+      //Sanity check that we are on the last word of the list.
+      expect(await homeScreen.currentWord, 'terima kasih');
+      expect(await homeScreen.currentDefinition, 'thank you');
+
+      //If we activate the next button we should get back to the start.
+      await homeScreen.tapNavigationButton();
+
+      expect(await homeScreen.currentWord, 'anda');
+      expect(await homeScreen.currentDefinition, '');
+      await homeScreen.tapNavigationButton();
+      expect(await homeScreen.currentWord, 'anda');
+      expect(await homeScreen.currentDefinition, 'you');
+    });
+
+    test('toggle show indonesian first', () async {
+      //Sanity check that word1 of list1 of chapter1 is showing.
+      expect(await homeScreen.currentWord, 'anda');
+
+      await homeScreen.tapIndonesianFirstToggleButton();
+
+      //The translation should now be showing as the word.
+      expect(await homeScreen.currentWord, 'you');
+      expect(await homeScreen.currentDefinition, '');
+
+      //Navigate through the list and check that English is in the
+      //word box and the Indonesian translation is in the definition
+      //box.
+      await homeScreen.tapNavigationButton();
+      expect(await homeScreen.currentWord, 'you');
+      expect(await homeScreen.currentDefinition, 'anda');
+      await homeScreen.tapNavigationButton();
+      await homeScreen.tapNavigationButton();
+      expect(await homeScreen.currentWord, 'what');
+      expect(await homeScreen.currentDefinition, 'apa');
     });
   });
 }
--- integration_tests/screens/main_screen.dart
+++ integration_tests/screens/main_screen.dart
@@ -1,10 +1,14 @@
-import 'package:flutter_driver/flutter_driver.dart';
 import 'dart:async';
-//import '../../lib/keys/keys.dart';
+
+import 'package:flutter_driver/flutter_driver.dart';
+
 import 'test_screen.dart';
+//import '../../lib/keys/keys.dart';
+
 
 class MainScreen extends TestScreen {
-//  final _listNavigationButton = find.byValueKey(navigateListButtonKey);
+  final _listNavigationButton = find.byValueKey('__navigate_list_button__');
+  final _indonesianFirstToggleButton = find.byValueKey('__indonesian_first_toggle_button__');
   final _wordArea = find.byValueKey('__word__');
   final _definitionArea = find.byValueKey('__definition__');
 
@@ -13,4 +17,12 @@ class MainScreen extends TestScreen {
   Future<String> get currentWord => driver.getText(_wordArea);
 
   Future<String> get currentDefinition => driver.getText(_definitionArea);
+
+  Future<Null> tapNavigationButton() async {
+    await driver.tap(_listNavigationButton);
+  }
+
+  Future<Null> tapIndonesianFirstToggleButton() async {
+    await driver.tap(_indonesianFirstToggleButton);
+  }
 }
